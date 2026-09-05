ef204a7d7517897afdd2afb7c069d840657e796c
Clartik <clartikdev@gmail.com>
2026-09-05 10:00:30 -0700

    feat: added close, minimize, and maximize functions for windowmanager

--- window.py
+++ window.py
@@ -1,13 +1,95 @@
+from typing import Optional
+
 import pywinctl as pwc
 
-# win = pwc.getWindowsWithTitle("Notepad", condition=pwc.Re.CONTAINS)[0]
+from intents import WindowIntent
 
-# win.activate()
+class WindowsManager:
+    @staticmethod
+    def _get_window(target: Optional[str]):
+        if target is not None:
+            windows = pwc.getWindowsWithTitle(target, condition=pwc.Re.CONTAINS, flags=pwc.Re.IGNORECASE)
+            
+            if len(windows) > 0:
+                window = windows[0]
+            else:
+                window = None
+        else:
+            window = pwc.getActiveWindow()
+            
+        return window
 
-# print(win.title, win.size, win.isMaximized, win.isAlive)
+    @staticmethod
+    def close(intent: WindowIntent):
+        if intent.action != 'close':
+            return
+        
+        window = WindowsManager._get_window(intent.target)
+        
+        if not window:
+            print('[WindowManager]: No window found!')
+            return None
+        
+        return window
+        
+    @staticmethod
+    def confirm_close(window):
+        if not window.isAlive:
+            return
+        
+        window.close()
+        
+        print(f'[WindowManager]: Closed `{window.title}` window!')
+            
+    @staticmethod
+    def minimize(intent: WindowIntent):
+        if intent.action != 'minimize':
+            return
+        
+        if intent.target != 'all':
+            window = WindowsManager._get_window(intent.target)
+            
+            if not window:
+                return
+            
+            window.minimize()
+            
+            print(f"[WindowManager]: Minimized first `{intent.target}` window!")       
+        else:
+            windows = pwc.getAllWindows()
+            
+            for window in windows:
+                if window.isMinimized:
+                    continue
+                
+                window.minimize()
+                
+            print("[WindowManager]: Minimized all windows!")       
 
-# from windows_tools.installed_software import get_installed_software
-
-# for software in get_installed_software():
-#     # print(software['name'], software['version'], software['publisher'])
-#     print(software)
+    @staticmethod
+    def maximize(intent: WindowIntent):
+        if intent.action != 'dock':
+            return
+        
+        if intent.position != 'full':
+            return
+        
+        if intent.target != 'all':
+            window = WindowsManager._get_window(intent.target)
+            
+            if not window:
+                return
+            
+            window.maximize()
+            
+            print(f"[WindowManager]: Maximized first `{intent.target}` window!")       
+        else:
+            windows = pwc.getAllWindows()
+            
+            for window in windows:
+                if window.isMaximized:
+                    continue
+                
+                window.maximize()
+                
+            print("[WindowManager]: Maximized all windows!")     
